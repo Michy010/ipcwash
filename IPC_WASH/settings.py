14cf46bcfa6bd6f26fc16e17282ad7b84f9827b3
Michy010 <michaelmsita17@gmail.com>
2023-10-23 12:37:55 +0300

    to deploy

--- IPC_WASH/settings.py
+++ IPC_WASH/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'IPC_WASH.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'blog/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
